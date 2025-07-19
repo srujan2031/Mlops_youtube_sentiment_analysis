7b320208daef3bb6f13768aa55582bbc4d91534a
srujan2031 <chintasrujan07@gmail.com>
2025-07-19 15:31:59 -0500

    updated code

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='mlops_youtube_sentiment',  # ← your project name
+    version='0.0.0',  # ← bump version if needed
+    author='Srujan Chinta',  # ← your name
+    author_email='chintasrujan07@gmail.com',  # ← your email
+    packages=find_packages(),
+    install_requires=[
+    ]
+)
